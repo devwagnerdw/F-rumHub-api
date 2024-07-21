326c6993524619c50603554e7ace043c82ceeb65
Vagne alves <vagnerdw1@gmail.com>
2024-07-21 15:19:53 -0300

    Refatoração do método de atualização e melhorias na verificação de permissões

--- src/main/java/forum/hub/api/controller/TopicoController.java
+++ src/main/java/forum/hub/api/controller/TopicoController.java
@@ -1,6 +1,5 @@
 package forum.hub.api.controller;
 
-
 import forum.hub.api.domain.topico.*;
 import forum.hub.api.domain.usuario.Usuario;
 import forum.hub.api.domain.usuario.UsuarioRepository;
@@ -9,26 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-
 import java.net.URI;
-import java.util.Date;
-import java.util.Optional;
-
-
 
 @RestController
 @RequestMapping("/topicos")
 public class TopicoController {
 
     @Autowired
-    UsuarioRepository usuarioRepository;
+    private UsuarioRepository usuarioRepository;
 
     @Autowired
     private TopicoRepository topicoRepository;
@@ -41,7 +35,6 @@ public class TopicoController {
     ) {
         Topico topico = new Topico(dados, logado);
         topicoRepository.save(topico);
-
         URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
         return ResponseEntity.created(uri).body(new DadosDetalhamentoTopico(topico));
     }
@@ -50,40 +43,38 @@ public class TopicoController {
     public Page<DadosListagemTopico> listar(
             @AuthenticationPrincipal Usuario logado,
             @PageableDefault(size = 10, sort = {"dataCriacao"}) Pageable paginacao) {
-
         return topicoRepository.findByUsuarioId(logado.getId(), paginacao).map(DadosListagemTopico::new);
     }
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity atualizar(@PathVariable Long id,@RequestBody @Valid DadosAtualizacaoTopico dados) {
-        Optional<Topico> topicoOptional = topicoRepository.findById(id);
-        if (!topicoOptional.isPresent()) {
-            return ResponseEntity.notFound().build();
-        }
-        var topico = topicoOptional.get();
-        topico.atualizarInformacoes(dados);
-        return ResponseEntity.ok(new DadosDetalhamentoTopico(topico));
+    public ResponseEntity atualizar(@PathVariable Long id, @RequestBody @Valid DadosAtualizacaoTopico dados, @AuthenticationPrincipal Usuario logado) {
+        return topicoRepository.findById(id)
+                .map(topico -> {
+                    if (!topico.getUsuario().getId().equals(logado.getId())) {
+                        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Você não tem permissão para atualizar este tópico.");
+                    }
+                    topico.atualizarInformacoes(dados);
+                    return ResponseEntity.ok(new DadosDetalhamentoTopico(topico));
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
     @Transactional
     public ResponseEntity excluir(@PathVariable Long id) {
-        Optional<Topico> topicoOptional = topicoRepository.findById(id);
-        if (!topicoOptional.isPresent()) {
-            return ResponseEntity.notFound().build();
-        }
-        topicoRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
+        return topicoRepository.findById(id)
+                .map(topico -> {
+                    topicoRepository.deleteById(id);
+                    return ResponseEntity.noContent().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity detalhar(@PathVariable Long id) {
-        Optional<Topico> topicoOptional = topicoRepository.findById(id);
-        if (!topicoOptional.isPresent()) {
-            return ResponseEntity.notFound().build();
-        }
-        var topico = topicoOptional.get();
-        return ResponseEntity.ok(new DadosDetalhamentoTopico(topico));
+        return topicoRepository.findById(id)
+                .map(topico -> ResponseEntity.ok(new DadosDetalhamentoTopico(topico)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
